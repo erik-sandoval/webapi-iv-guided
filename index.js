f0ad82598920c65d5fa22c8da7a1b9d5e1af7558
erik-sandoval <erik.sandoval@icloud.com>
2019-05-30 10:35:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
-
-
